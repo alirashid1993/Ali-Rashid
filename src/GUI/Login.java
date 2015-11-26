34b216e0a1db2c8770ce354db748bd78bd465f40
Alirashid1993 <ali_rashid006@hotmail.com>
2015-11-26 14:40:46 +0100

    Tilfoejet kode fra Login Eclipse og overfoert den til intelliJ

--- src/GUI/Login.java
+++ src/GUI/Login.java
@@ -1,7 +1,111 @@
 package GUI;
 
-/**
- * Created by AliRashid on 26/11/15.
- */
-public class Login {
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+
+public class Login extends JPanel {
+    private JTextField username;
+    private JPasswordField password;
+    private JButton btnLogin;
+    private JLabel lblWelcome;
+    private JLabel lblPlease;
+    private JLabel lblUsername;
+    private JLabel lblPassword;
+
+
+
+    /**
+     * Create the panel.
+     */
+    public Login()
+    {
+        setLayout(null);
+        setBounds(100, 100, 550, 400);
+
+        btnLogin = new JButton("Login");
+
+        password = new JPasswordField();
+        password.setBounds(231, 242, 153, 26);
+        add(password);
+
+        lblWelcome = new JLabel("to Snake");
+        lblWelcome.setFont(new Font("Phosphate", Font.PLAIN, 52));
+        lblWelcome.setBounds(168, 127, 218, 41);
+        add(lblWelcome);
+
+        lblPlease = new JLabel("Please enter Username and Password:");
+        lblPlease.setFont(new Font("Impact", Font.PLAIN, 13));
+        lblPlease.setBounds(166, 180, 218, 16);
+        add(lblPlease);
+
+        lblUsername = new JLabel("Username:");
+        lblUsername.setFont(new Font("Impact", Font.PLAIN, 13));
+        lblUsername.setBounds(168, 214, 71, 16);
+        add(lblUsername);
+
+        lblPassword = new JLabel("Password:");
+        lblPassword.setFont(new Font("Impact", Font.PLAIN, 13));
+        lblPassword.setBounds(168, 246, 71, 16);
+        add(lblPassword);
+
+        username = new JTextField();
+        username.setBounds(233, 208, 153, 28);
+        add(username);
+        username.setColumns(10);
+
+        btnLogin.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+        btnLogin.setFont(new Font("Impact", Font.PLAIN, 22));
+        btnLogin.setBounds(166, 274, 218, 41);
+        add(btnLogin);
+
+        JLabel lblWelcome_1 = new JLabel("Login");
+        lblWelcome_1.setFont(new Font("Phosphate", Font.PLAIN, 52));
+        lblWelcome_1.setBounds(201, 73, 152, 45);
+        add(lblWelcome_1);
+
+    }
+
+
+    public JTextField getEnterusername() {
+        return username;
+    }
+
+    public void setEnterusername(JTextField enterusername) {
+        this.username = enterusername;
+    }
+
+    public JTextField getUsername() {
+        return username;
+    }
+
+    public void setUsername(JTextField username) {
+        this.username = username;
+    }
+
+
+    public JPasswordField getPassword() {
+        return password;
+    }
+
+
+    public void setPassword(JPasswordField password) {
+        this.password = password;
+    }
+
+
+    public void actionPerformedLogin(ActionListener l) {
+        btnLogin.addActionListener(l);
+    }
 }
